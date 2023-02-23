f7d6796710c366759774e40944e8c0220745232c
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-02-23 12:59:15 -0500

    starting setup

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="container">
+    <h2>{{ userName }}</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: 'Nikola',
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+.container {
+  padding: 1rem;
+  margin: 3rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  text-align: center;
+}
+</style>
